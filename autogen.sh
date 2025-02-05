f12bc969afe54520a2ed1df93ac73a0fab0e0fec
hackleberry <griffinschreiber@gmail.com>
2025-02-05 20:24:46 +0000

    Created autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Please build cglm and put the .a file in the lib directory or this script will not work."
+
+cd $(dirname "$0")
+
+echo "Installing and updating submodules..."
+git submodule init
+git submodule update
+
+echo "Generating files and building..."
+autoheader
+aclocal
+autoconf
+automake --add-missing --copy
+
+echo "Done!"
